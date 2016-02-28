96fbb8a5ebb9946163257684adaf856db4a84219
Mike <mike.vercoelen@gmail.com>
2016-02-28 04:43:02 +0100

    Update 2016-02-28T03:43:00.771Z

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
-import { TransitionPortal } from '../../'
+import createRemodal from '../../'
 
-console.log(TransitionPortal)
+const Remodal = createRemodal()
 
 class App extends Component {
+  state = {
+    isModalOpen: false
+  }
+
+  toggleModal () {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    })
+  }
+
   render () {
     return (
       <div>
-        <h1>This is my fucking homepage.</h1>
+        <button onClick={::this.toggleModal}>
+          Open modal
+        </button>
+        <Remodal isOpen={this.state.isModalOpen} onClose={::this.toggleModal} overlayClosesModal>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </Remodal>
       </div>
     )
   }
